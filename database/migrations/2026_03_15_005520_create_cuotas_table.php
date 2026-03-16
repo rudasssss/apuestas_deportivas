e9f77260dcbde19eccb1bffff80e6efb6e3c96fb
Sebastian Arias <juansearias21@gmail.com>
2026-03-15 21:45:13 -0500

    Create cuotas migration  anadido

---
+++ database/migrations/2026_03_15_005520_create_cuotas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCuotasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cuotas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->foreignId('evento_id')
+                  ->constrained('eventos')
+                  ->onDelete('cascade');
+
+            $table->string('tipo_apuesta');
+            $table->decimal('cuota', 4, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cuotas');
+    }
+}
