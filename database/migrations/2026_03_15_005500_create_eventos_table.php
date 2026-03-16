c5b5591921dbdd31159101189efc28d54fc5d7dc
Trujirendjj <juanj.trujillor@autonoma.edu.co>
2026-03-15 21:31:44 -0500

    Subiendo createEventos

---
+++ database/migrations/2026_03_15_005500_create_eventos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eventos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('deporte');
+            $table->string('equipo_local');
+            $table->string('equipo_visitante');
+            $table->date('fecha');
+
+            $table->string('estado')->default('pendiente');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eventos');
+    }
+}
